51d8c6f478d3ea72988db5a6cf3eb3e67605cde2
Francisco Bórquez <fborquez@outlook.com>
2022-02-26 22:16:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const httppolicyfactory = require('@fjborquez/httppolicies');
+
+(function() {
+    var policy = {};
+
+    function setPolicyHeader(options, req, res, next) {
+        policy = httppolicyfactory(options);
+        res.set(createHeader(options, policy));
+        next();
+    }
+
+    function createHeader(options, policy) {
+        const header = {};
+        header[policy.getHeaderName()] = policy.getDirectiveOrDefault(options.directive);
+        return header;
+    }
+
+    module.exports = (options) => {
+        return (req, res, next) => setPolicyHeader(options, req, res, next)
+    };
+})();
+
+// TODO: add a test for this middleware
